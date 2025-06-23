8ad365eda1524c76c9bede151d2617a537b29a5a
sudip <sudeeplamichhane18@gmail.com>
2025-06-23 14:46:50 +0545

    updated sitemap config

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,9 +1,47 @@
-// No need to import anything — just export the config object
-
 /** @type {import('next-sitemap').IConfig} */
+const siteUrl = 'https://heartcrop.netlify.app';
+
 const config = {
-  siteUrl: 'https://heartcrop.netlify.app', // 🔁 Change when you buy a domain
+  siteUrl,
   generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ['/404'],
+  i18n: {
+    locales: ['en', 'de', 'es', 'fr', 'hi', 'it', 'pt'], // your locales here
+    defaultLocale: 'en',
+  },
+  alternateRefs: [
+    {
+      href: `${siteUrl}/en`,
+      hreflang: 'en',
+    },
+    {
+      href: `${siteUrl}/de`,
+      hreflang: 'de',
+    },
+    {
+      href: `${siteUrl}/es`,
+      hreflang: 'es',
+    },
+    {
+      href: `${siteUrl}/fr`,
+      hreflang: 'fr',
+    },
+    {
+      href: `${siteUrl}/hi`,
+      hreflang: 'hi',
+    },
+    {
+      href: `${siteUrl}/it`,
+      hreflang: 'it',
+    },
+    {
+      href: `${siteUrl}/pt`,
+      hreflang: 'pt',
+    },
+  ],
 };
 
 export default config;
